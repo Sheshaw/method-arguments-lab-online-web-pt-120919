787df54d562cafd48c6e43002d783c4de0287865
Chayan Martinez <nayachmartinez68@gmail.com>
2019-12-12 06:17:38 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,8 @@
-name = "Josh"
-language = "Ruby"
 
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning #{language}!"
+def introduction_with_language("Josh", "Python")
+  puts "Hi, my name is #{"Josh"} and I am learning to program in #{"Python"}!\n"
 end
 
 introduction_with_language(name, language)
+
+# this is the proper way through the learn.co lessons so I am re-submitting it.
