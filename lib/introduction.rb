101a2d7f1433faecdbc5bfc3c74df5f2a4c79e2e
Chayan Martinez <nayachmartinez68@gmail.com>
2020-01-17 09:06:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,10 @@
 
-def introduction_with_language("Josh", "Python")
-  puts "Hi, my name is #{"Josh"} and I am learning to program in #{"Python"}!\n"
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}!\n"
 end
 
-introduction_with_language(name, language)
+introduction_with_language("Josh", "Python")
+
 
 # this is the proper way through the learn.co lessons so I am re-submitting it.
