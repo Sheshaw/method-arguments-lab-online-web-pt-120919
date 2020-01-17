44a15a702680e580c521334f78bf3f10d6a6994e
Chayan Martinez <nayachmartinez68@gmail.com>
2020-01-17 09:11:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}!\n"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
 introduction_with_language("Dan", "Ember.js")
